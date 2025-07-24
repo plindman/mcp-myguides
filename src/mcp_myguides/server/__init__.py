a921bf84604b0f462dd6752dedb8af7d0cf13770
Pontus Lindman <pontus.v.lindman@gmail.com>
2025-07-24 11:47:38 +0300

    Server code refactored

--- src/mcp_myguides/server/__init__.py
+++ src/mcp_myguides/server/__init__.py
@@ -1,7 +1,2 @@
 from .server import get_app
-from .components_guides import app as components_guides
-from .tool_health import app as tool_health
-
-mcp = get_app()
-
-__all__ = ["mcp", "get_app"]
+__all__ = ["get_app"]
